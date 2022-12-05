c7df5df7af381b8437f52caa747ce234f5828ebf
lauro <lauropimentel12@gmail.com>
2022-12-05 11:29:34 -0300

    api

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: "https://bingoshow.fly.dev",
-    baseURL: "http://localhost:8000",
+    baseURL: "https://bingoshow.fly.dev",
+    //baseURL: "http://localhost:8000",
     headers: { "Content-Type": "application/json" },
   });
   
